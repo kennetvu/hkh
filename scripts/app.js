565314a9e897519dcadb946adf4dc97e80f8e34e
Kennet Vuong <Kennetvuong@gmail.com>
2013-11-07 14:48:50 +0100

    Fixed userinfo the right way

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,9 @@
+/*
+* Root file for our angular app!
+* Written by hkh
+* NB!!! Remember to change URLINFO if you're using another app than dhis/demo
+*/
+
 var surveyApp = angular.module('surveyApp', [
 	//'ngRoute'
 	]);
@@ -6,20 +12,20 @@ surveyApp.config(function($routeProvider){
 	$routeProvider
 	.when('/',
 	{
-		templateUrl:"view/home.html"
+		templateUrl:"views/home.html"
 	})
 
 	.when('/home',{
-		templateUrl: 'view/home.html'
+		templateUrl: 'views/home.html'
 	})
 	.when('/form',{
-		templateUrl: 'view/form.html'
+		templateUrl: 'views/form.html'
 	})
 	.when('/entry',{
-		templateUrl: 'view/entry.html'
+		templateUrl: 'views/entry.html'
 	})
 	.when('/survey',{
-		templateUrl: 'view/survey.html'
+		templateUrl: 'views/survey.html'
 	})
 	.otherwise({
 		rederictTo: '/home'
@@ -28,11 +34,62 @@ surveyApp.config(function($routeProvider){
 	}//Function, routeProvider
 	);
 
-surveyApp.factory('userInfo', function(){
-	var username = '';
+/*
+* Factory to get urlInfo. Should be injected everytime we need to talk with restful
+* REMEMBER TO CHANGE ELSE PART IF USING ANOTHER URLS!!!!
+* Returns oppsal if current host is localhost
+* Returns right path to oppsal if host is oppsal
+* Returns DHIS2/DEMO else. 
+* This factory returns without backslash
+*/
+surveyApp.factory('urlInfo', function($location){
+	//console.log("From urlInfo " + $location.absUrl());
+	//console.log("host: " + $location.host());
+	// Check if host is localhost, if yes, get url from else where.
+	//Load from file? 
+	if($location.host() == "localhost"){
+		return $location.host();
+//		return "http://oppsal.dyndns.info:8080/dhis";
+}
+else{
+		//If host is oppsal. remember to add port.
+		if($location.host() == "oppsal.dyndns.info"){
+			return "http://oppsal.dyndns.info:8080/dhis";
+		}// If dhis, just return url info? Remember to change this.
+		else{
+			return "http://apps.dhis2.org/demo";
+		}
+	}
+	
+});
+/*Descrive a user.*/
+surveyApp.factory('userInfo', function($http, urlInfo){
+	var username = 'Not logged in';
 	var firstname = '';
 	var lastname = '';
-	var loggedIn = false;
+
+
+	//tmp solution:
+	var url = '';
+	console.log(urlInfo);
+	if(urlInfo == "localhost"){
+		url = 'data/me.json';
+	}
+	else{
+		url = urlInfo + '/api/me.json';
+	}
+	$http.get(url)
+	.success(function(data){
+		//console.log(data);
+		username = data.userCredentials.username;
+		firstname = data.firstName;
+		lastname = data.surname;
+
+
+	})
+	.error(function(response, status){
+		console.log(response + status);
+	});
 	return {
 		getUsername: function(){return username;},
 		setUsername: function(name){
@@ -46,35 +103,13 @@ surveyApp.factory('userInfo', function(){
 		getLastname: function(){return lastname;},
 		setLastname: function(name){
 			lastname = name;
-		},
-
-		getLoggedIn: function(){return loggedIn;},
-		setLoggedIn: function(status){loggedIn = status;}
+		}
 	};
 });
 
 //RUn this after controller is started
-surveyApp.controller('MainController', function($scope, $http, userInfo){
-	//var url = "http://apps.dhis2.org/demo/api/me.json";
-	var url = "http://oppsal.dyndns.info:8080/dhis/api/me.json";
-	$.getJSON( url, function( data ) {
-	//	console.log(data.userCredentials.username);
-		userInfo.setUsername(data.userCredentials.username);
-		userInfo.setFirstname(data.firstName);
-		userInfo.setLastname(data.surname);
-		userInfo.setLoggedIn(true);
-		//console.log(userInfo.getLoggedIn())
-	}).fail(function(jqxhr, textStatus, error){
-		console.log("fail");
-		var err = textStatus + ", " + error;
-		console.log( "Request Failed: " + err );
-	});
-	$http.get('http://oppsal.dyndns.info:8080/dhis/api/me').then(function(response){
-		//console.log(response);
-	});
-	console.log("after query");	
+surveyApp.controller('MainController', function($scope, $http, userInfo, urlInfo){
 	
-
 
 });
 surveyApp.controller('DebugController', function($scope, $location){
--- scripts/controllers/userinfoController.js
+++ scripts/controllers/userinfoController.js
@@ -1,23 +1,17 @@
 var surveyApp = angular.module('surveyApp');
 
+/*User should always be logged in, since the program is running through DHIS.*/
 surveyApp.controller('UserinfoController', function($scope, $http, userInfo){
-
 	$scope.ButtonClick = function(){
 		alert(userInfo.getFirstname());
 	};
-	console.log(userInfo.getLoggedIn());
 	//userInfo.setLoggedIn(false);
-	if(userInfo.getLoggedIn() === false){
-		$scope.username = "Not logged in";
-		$scope.show = false;
-		return;
-	}
-	else{
-		$scope.show = true;
-		$scope.username = userInfo.getUsername();
-		$scope.firstname = userInfo.getFirstname();
-		$scope.lastname = userInfo.getLastname();
-	}
+	$scope.username = "Not logged in";
+	$scope.show = true;
+	$scope.username = userInfo.getUsername();
+	$scope.firstname = userInfo.getFirstname();
+	$scope.lastname = userInfo.getLastname();
+
 /*
 	$scope.$watch('userInfo.getLoggedIn()', function (newdata){
 		console.log(newdata);
